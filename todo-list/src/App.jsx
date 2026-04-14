438b9bca083f59c1959e05d82cbbf709e759d11f
Shantelwi <williamsshantel35@yahoo.com>
2026-04-14 08:04:00 -0700

    fixed vite issue

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   const todoList = [
     {id: 1, title: "review resources"},
     {id: 2, title: "take notes"},
-    {id: 3, title: " code out app"},
+    {id: 3, title: "code out app"}
   ]
-  
+
   return (
     <div>
       <h1>My Todos</h1>
@@ -14,7 +14,6 @@ function App() {
         {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
       </ul>
     </div>
-      
   )
 }
 
