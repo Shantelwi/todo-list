a33f42fb40d6a40e9137851f280ecde26cef7e82
Shantelwi <williamsshantel35@yahoo.com>
2026-05-06 05:20:12 -0700

    Finished the second half of the assignment

--- todo-list/src/TodoForm.jsx
+++ todo-list/src/TodoForm.jsx
@@ -1,16 +1,15 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
  //create form submit handler
 function TodoForm({ onAddTodo }) {
     const inputRef = useRef();
-
+    const [workingTodoTitle, setWorkingTodoTitle] = useState('');
     const handleAddTodo = (event) => {
         event.preventDefault();
 
-        const todoTitle = event.target.todoTitle.value.trim();
-        if (todoTitle && todoTitle !== "") {
-            onAddTodo(todoTitle);
-            event.target.reset();
+        if (workingTodoTitle.trim() !== "") {
+            onAddTodo(workingTodoTitle);
+            setWorkingTodoTitle('');
             inputRef.current.focus();
         }
     };
@@ -22,10 +21,12 @@ function TodoForm({ onAddTodo }) {
                 type = "text" 
                 id = "todoTitle"
                 name = "todoTitle"
-                placeholder = {'Todo text'}
+                value = {workingTodoTitle}
+                onChange  = {(e) =>  setWorkingTodoTitle(e.target.value) }
+                placeholder = 'Todo text'
                 required 
             />
-            <button type="submit"> Add Todo </button>
+            <button type="submit" disabled = {!workingTodoTitle.trim()} > Add Todo </button>
         </form>
     );
 }
--- todo-list/src/TodoList.jsx
+++ todo-list/src/TodoList.jsx
@@ -1,12 +1,12 @@
 import TodoListItem from './TodoListItem.jsx'
 
-function TodoList({todoList}){
+function TodoList({todoList, onCompleteTodo}){
     const filteredTodoList = todoList.filter(
         (todo) => !todo.isCompleted
     );
     return (
         <ul>
-        {filteredTodoList.map(todo => <TodoListItem key={todo.id} todo={todo} onCompletedTodo={onCompletedTodo}/>)}
+        {filteredTodoList.map(todo => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/>)}
         </ul>
     );
 }
