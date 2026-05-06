fa46df52f30d30297421da30fcde5c7c1f99fb9d
Shantelwi <williamsshantel35@yahoo.com>
2026-05-06 11:43:35 -0700

    fixed errors

--- todo-list/src/TodoList.jsx
+++ todo-list/src/TodoList.jsx
@@ -1,11 +1,20 @@
 import TodoListItem from './TodoListItem.jsx'
 
-function TodoList({todoList}){
-    
+function TodoList({todoList, onCompleteTodo}){
+    const filteredTodoList  = todoList.filter(
+        (todo) => !todo.isCompleted
+    );
+
     return (
-        <ul>
-        {todoList.map(todo => <TodoListItem key={todo.id} todo={todo}/>)}
-        </ul>
+        filteredTodoList.length === 0 ? (
+            <p>Add todo above to get started</p>
+        ) : (
+
+            <>
+                {filteredTodoList.map((todo) => 
+                <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/>)}
+            </>
+        )
     );
 }
 
