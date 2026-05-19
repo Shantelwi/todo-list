69eefe061c73ec07ee3c31cce8d6eb211caa301c
Shantelwi <williamsshantel35@yahoo.com>
2026-05-19 08:11:41 -0700

    lesson-05-issues

--- todo-list/src/TodoListItem.jsx
+++ todo-list/src/TodoListItem.jsx
@@ -1,13 +1,8 @@
 
 //Destructured
-function TodoListItem({todo, onCompleteTodo}){
+function TodoListItem({todo}){
     return (
         <li>
-            <input 
-            type="checkbox"
-            checked={todo.isCompleted}
-            onChange={()  => onCompleteTodo(todo.id)} 
-            />
             {todo.title}
         </li>
     );
