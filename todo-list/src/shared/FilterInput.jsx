9542597ff605beda1de484000a81161329506d43
Shantel Williams <williamsshantel73@gmail.com>
2026-05-26 20:32:15 +0000

    added filter input file

---
+++ todo-list/src/shared/FilterInput.jsx
@@ -0,0 +1,15 @@
+export default function FilterInput({filterTerm, onFilterChange}) {
+    return (
+        <div>
+            <label htmlFor="filterInput">Search todos:</label>
+
+            <input 
+                type="text"
+                id="filterInput"
+                value={filterTerm}
+                onChange={(e) => onFilterChange(e.target.value)}
+                placeholder="Search by title..."
+             />
+        </div>
+    );
+}
